0debfe94f6071921170f85ff2fc89e303437f744
Jean Carlo <jean98vb@hotmail.com>
2016-11-03 11:46:37 -0600

    First Change

--- FeedMeApi/FeedMeApi/ModelDB.Designer.cs
+++ FeedMeApi/FeedMeApi/ModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\JeanCarlo\Documents\MEGA\Cursos\Visual Studio\FeedMeApi\FeedMeApi\ModelDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\JeanCarlo\Documents\GitHub\FeedMe\FeedMeApi\FeedMeApi\ModelDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
